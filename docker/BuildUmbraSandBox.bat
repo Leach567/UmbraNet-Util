5fb8397c1cebaba4e32d60bc9acb70fd48f37983
NLeach <nick.p.leach@gmail.com>
2022-09-15 02:21:57 -0400

    Adding windows docker wrapper scripts
    Signed-off-by: NLeach <nick.p.leach@gmail.com>

---
+++ docker/BuildUmbraSandBox.bat
@@ -0,0 +1,1 @@
+docker image build -t umbra_nn_sand_gpu:v0 -f ./docker/Umbranet_NN_Sandbox_gpu.dok .
---
+++ docker/StartUmbraSandBox.bat
@@ -0,0 +1,5 @@
+docker container run -it --rm  ^
+-v "C:\Users\nickp\OneDrive\Documents\Comp_Sci\Projects\AI\UmbraNet":"/apps" ^
+-v "C:\Users\nickp\OneDrive\Documents\Comp_Sci\Projects\AI\UmbraCC_testData":"/data" ^
+-p 80:80 ^
+umbra_nn_sand_gpu:v0
