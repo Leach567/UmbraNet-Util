2e920717bfe7f37a7d9e0a75aa93d8557ab4c173
NLeach <nick.p.leach@gmail.com>
2025-01-21 20:47:46 -0500

    Updated

--- build/build_workspace.bash
+++ build/build_workspace.bash
@@ -18,14 +18,14 @@ echo "Linking MODELS"
 mkdir -p Models
 $UMBRA_BUILD_SCRIPT_ROOT/clone.bash $UMBRA_MODELS_REPO_ROOT $PWD/Models
 
-if [ ! -e $UMBRA_MODELS_REPO_ROOT/Umbra_Cpp/config.bash ]
+if [ ! -e $UMBRA_MODELS_REPO_ROOT/config.bash ]
 then
     echo "Cannot find Model config script. Terminating."
     exit 1
 fi
 
-echo "Linking Umbra_Cpp Dependencies..."
-. $UMBRA_MODELS_REPO_ROOT/Umbra_Cpp/config.bash
+echo -e "\tLinking Umbra_Cpp Dependencies..."
+. $UMBRA_MODELS_REPO_ROOT/config.bash
 
 _modelDepRoot="$UMB_MODEL_CPP_ROOT/dep"
 mkdir -p $_modelDepRoot
@@ -33,5 +33,13 @@ mkdir -p $_modelDepRoot
 _modelDepRoot="$UMB_MODEL_CPP_ROOT/dep/Boost"
 mkdir -p $_modelDepRoot
 $UMBRA_BUILD_SCRIPT_ROOT/clone.bash $UMBRA_BOOST_REPO_ROOT $_modelDepRoot
+
+echo "Linking PROJECTS"
+_projectRoot="$UMBRA_DEV_ROOT/Projects"
+mkdir -p $_projectRoot
+
+pushd $UMBRA_DEV_ROOT/Projects > /dev/null/
+$UMBRA_BUILD_SCRIPT_ROOT/clone.bash $UMBRA_PROJECTS_REPO_ROOT $_projectRoot
+popd > /dev/null
 
 popd > /dev/null
