9d84863ea244d692544b7666e000cd81f468b558
Umbra <umbra@umbra-blade.com>
2026-09-04 08:54:24 -0400

    Adding Plex Server for PS5 Streaming access.

--- docker/docker_env_config.umbra.bash
+++ docker/docker_env_config.umbra.bash
@@ -1,8 +1,16 @@
 #!/bin/bash
 SERVICE_REPO_ROOT="/repos/UmbraNet-Util/docker"
+function force_reset_docker(){
+	sudo systemctl stop docker
+	sudo rm -f /var/lib/docker/network/files/local-kv.db
+	sudo systemctl start docker
+}
+export -f force_reset_docker
+
 function start_umbra_service(){
 	pushd ${SERVICE_REPO_ROOT}/$1
 	
+	echo "Starting $1"
 	docker compose up -d
 
 	popd
@@ -12,6 +20,7 @@ export -f start_umbra_service
 function stop_umbra_service(){
 	pushd ${SERVICE_REPO_ROOT}/$1
 	
+	echo "Stopping $1"
 	docker compose down --remove-orphans
 
 	popd
@@ -30,12 +39,14 @@ export -f restart_umbra_service
 
 SERVICE_INIT_ORDER=( 
 "PiHole"
+"Nginx"
+"Dozzle"
 "UptimeKuma"
 "HomePage"
-"Dozzle"
-"Nginx"
 "Trillium"
 "JellyFin"
+"Plex"
+"VSCode"
 "HomeAssistant"
 "VaultWarden"
 )
@@ -44,13 +55,23 @@ function start_umbra_services(){
 	pushd ${SERVICE_REPO_ROOT}/$1
 	
 	for dir in ${SERVICE_INIT_ORDER[@]}; do
-		echo "Starting $dir"
-		#start_umbra_service ${dir}
+		start_umbra_service ${dir}
 	done
 
 	popd
 }
 export -f start_umbra_services
+
+function stop_umbra_services(){
+	pushd ${SERVICE_REPO_ROOT}/$1
+	
+	for dir in ${SERVICE_INIT_ORDER[@]}; do
+		stop_umbra_service ${dir}
+	done
+
+	popd
+}
+export -f stop_umbra_services
 
 function restart_umbra_services(){
 	pushd ${SERVICE_REPO_ROOT}/$1
